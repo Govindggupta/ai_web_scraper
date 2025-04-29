e3919239167f073ff65e39d58c2aaae5c66e2a19
Govind Gupta <govind12771277@gmail.com>
2025-04-29 22:01:02 +0530

    setup

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from ai-web-scraper!")
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
 
 
-if __name__ == "__main__":
-    main()
+client = OpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+# Create a prompt
+response = client.chat.completions.create(
+    model="gemini-2.0-flash",
+    response_format={"type": "json_object"},
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "What is the capital of France?"},
+    ]
+)
+
+print(response.choices[0].message.content)
